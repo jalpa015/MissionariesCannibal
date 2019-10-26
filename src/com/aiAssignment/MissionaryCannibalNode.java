8c4eeec9e06b8f2bc405d4515355da16852cfca0
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-10-26 21:06:14 +0530

    * added TODO comments for MissionaryCannibalNode.java * updated .gitignore * added TODO sections for readme

--- src/com/aiAssignment/MissionaryCannibalNode.java
+++ src/com/aiAssignment/MissionaryCannibalNode.java
@@ -5,40 +5,57 @@ import java.util.Arrays;
 
 public class MissionaryCannibalNode extends DFSAndIDS {
 
-    // the following are the only instance variables you should need.
-    // (some others might be inherited from UUSearchProblem, but worry
-    // about that later.)
-
-    private int goalm, goalc, goalb;
-    private int totalMissionaries, totalCannibals;
+    /**
+     * The following are the only instance variables you should need.(some others might be inherited from
+     * UUSearchProblem, but worry about that later.)
+     * <p>
+     * TODO what is goalm here? Write comments here
+     */
+    private int goalm;
+    /**
+     * TODO what is goalc here? Write comments here
+     */
+    private int goalc;
+    /**
+     * TODO what is goalb here? Write comments here
+     */
+    private int goalb;
+    /**
+     * TODO what is totalMissionaries here? Write comments here
+     */
+    private int totalMissionaries;
+    /**
+     * TODO what is totalCannibals here? Write comments here
+     */
+    private int totalCannibals;
 
     public MissionaryCannibalNode(int sm, int sc, int sb, int gm, int gc, int gb) {
-
         startNode = new CannibalNode(sm, sc, sb, 0);
         goalm = gm;
         goalc = gc;
         goalb = gb;
         totalMissionaries = sm;
         totalCannibals = sc;
-
     }
 
-    // node class used by searches. Searches themselves are implemented
-    // in UUSearchProblem.
+    /**
+     * Node class used by searches. Searches themselves are implemented in UUSearchProblem.
+     */
     private class CannibalNode implements Node {
-
-        // do not change BOAT_SIZE without considering how it affect
-        // getSuccessors.
-
+        /**
+         * Do not change BOAT_SIZE without considering how it affect getSuccessors.
+         */
         private final static int BOAT_SIZE = 2;
 
-        // how many missionaries, cannibals, and boats
-        // are on the starting shore
+        /**
+         * How many missionaries, cannibals, and boats are on the starting shore
+         */
         private int[] state;
 
-        // how far the current node is from the start. Not strictly required
-        // for search, but useful information for debugging, and for comparing
-        // paths
+        /**
+         * How far the current node is from the start. Not strictly required for search, but useful information for
+         * debugging, and for comparing paths
+         */
         private int depth;
 
         public CannibalNode(int m, int c, int b, int d) {
@@ -47,14 +64,12 @@ public class MissionaryCannibalNode extends DFSAndIDS {
             this.state[1] = c;
             this.state[2] = b;
             depth = d;
-
         }
 
+        /**
+         * Add actions (denoted by how many missionaries and cannibals to put in the boat) to current state.
+         */
         public ArrayList<DFSAndIDS.Node> getSuccessors() {
-            // add actions (denoted by how many missionaries and cannibals to
-            // put
-            // in the boat) to current state.
-
             ArrayList<Node> list = new ArrayList<Node>();
             int m = this.state[0];
             int c = this.state[1];
@@ -128,10 +143,10 @@ public class MissionaryCannibalNode extends DFSAndIDS {
                     list.add(new CannibalNode(m, c + 2, b, depth + 1));
             }
 
-            for(int i = 0; i <= BOAT_SIZE; i++){
-                for(int j = 0; j <= BOAT_SIZE; j++){
-                    if (isSafeState(i,j))
-                        list.add(new CannibalNode(i,j,b, depth + 1));
+            for (int i = 0; i <= BOAT_SIZE; i++) {
+                for (int j = 0; j <= BOAT_SIZE; j++) {
+                    if (isSafeState(i, j))
+                        list.add(new CannibalNode(i, j, b, depth + 1));
                 }
             }
 
@@ -139,33 +154,42 @@ public class MissionaryCannibalNode extends DFSAndIDS {
             return list;
         }
 
+        /**
+         * TODO write comments here
+         * @param a
+         * @param b
+         * @return
+         */
         private boolean isSafeState(int a, int b) {
-            if (a > totalMissionaries || b > totalCannibals){
-
+            if (a > totalMissionaries || b > totalCannibals) {
                 return false;
             }
 
-            if (a < 0 || b < 0){
-
+            if (a < 0 || b < 0) {
                 return false;
             }
 
-            if (a < b && a != 0){
-
+            if (a < b && a != 0) {
                 return false;
             }
 
             return (totalMissionaries - a) >= (totalCannibals - b) || (totalMissionaries - a) == 0;
-
         }
 
+        /**
+         * TODO write comments here
+         * @return
+         */
         @Override
         public boolean goalTest() {
             return (this.state[0] == goalm && this.state[1] == goalc && this.state[2] == goalb);
         }
 
-        // an equality test is required so that visited lists in searches
-        // can check for containment of states
+        /**
+         * An equality test is required so that visited lists in searches can check for containment of states
+         * @param other
+         * @return
+         */
         @Override
         public boolean equals(Object other) {
             return Arrays.equals(state, ((CannibalNode) other).state);
@@ -180,9 +204,8 @@ public class MissionaryCannibalNode extends DFSAndIDS {
         public String toString() {
             String output = "(" + this.state[0]
                     + "," + this.state[1] + ","
-                    + this.state[2]+")";
+                    + this.state[2] + ")";
             return output;
-
         }
 
         @Override
